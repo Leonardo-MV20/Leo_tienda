9fe3c17571ebd15839dcf461dc19806ae44cd030
Leonardo-MV20 <leomoscoav20@gmail.com>
2026-04-10 00:20:29 -0600

    Commit semana 11

---
+++ src/main/java/Leo_tienda/tienda/repository/RolRepository.java
@@ -0,0 +1,11 @@
+package Leo_tienda.tienda.repository;
+
+import Leo_tienda.tienda.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
